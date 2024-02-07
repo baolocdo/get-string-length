a405a71d8056027c9ddd42b3dc332e90b64f7446
meredithGatlin <159324773+meredithGatlin@users.noreply.github.com>
2024-02-07 11:29:21 -0800

    Update get-string-length.js

--- get-string-length.js
+++ get-string-length.js
@@ -3,7 +3,7 @@ let myString = 'Hello World!!!';
 // TODO: get the size of `myString` defined above and assign to `len`.
 // NOTE: DO NOT HARD CODE THE SIZE OF `myString`. When you change the value
 // of `myString`, the test should still pass.
-let len = -1;
+let len = myString.length;
 
 // DO NOT CHANGE ANYTHING BELOW THIS LINE.
 
